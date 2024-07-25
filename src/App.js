142bf6ab712285990c6a3ef72706f656d8867011
shun ishii <shun_ishii@shunnoMacBook-puro.local>
2024-07-25 12:53:24 +0900

    “2024-07-25”

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-function MyButton() {
-  return (
-    <button>
-      I'm a button
-    </button>
-  );
-}
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
 
-export default function MyApp() {
+export default function Profile() {
   return (
-    <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
-    </div>
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
   );
 }
