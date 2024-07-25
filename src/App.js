dd75c05f88e0455c521f021ed758e425f2971b28
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-07-25 13:13:04 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-function MyButton() {
-  return (
-    <button>
-      I'm a button
-    </button>
-  );
-}
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
 
-export default function MyApp() {
+export default function ShoppingList() {
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+    </li>
+  );
+
   return (
-    <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
-    </div>
+    <ul>{listItems}</ul>
   );
 }
